dc941732481b7c3429c5e9343cc036e1a64cf7dd
james <james.coates@me.com>
2019-11-05 19:18:05 +0000

    correct menu links

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -7,8 +7,11 @@ const About = () => {
     <section className={aboutStyles.section} id="about">
       <div className={aboutStyles.container}>
         <img  className={aboutStyles.image} src={require('../../images/james.png')} />
-        <h3>Hey</h3>
-        <p>I’m James a developer and  engineer based in the UK. I build all sorts from web apps to something else.</p>
+        <h3>Hey,</h3>
+        <p>My name is James and I'm a developer and engineer based in the 
+          UK. I love to tinker, design and build things.</p>
+        <p>I'm passionate about Bitcoin and public blockchain tech, with a keen 
+          interest in global macroeconomics.</p>
       </div>
     </section>
   );
--- src/components/menu-screen/index.js
+++ src/components/menu-screen/index.js
@@ -11,20 +11,20 @@ const MenuScreen = ({ menuActive, setMenuActive }) => {
   <div className={`${MenuScreenStyles.wrapper} ${menuActive ? MenuScreenStyles.menuActive : null}`}>
     <div className={MenuScreenStyles.col1}>
       <div className={MenuScreenStyles.nav}>
-        <Link to="/projects" onClick={() => setMenuActive()}>
+        <Link to="/#about" onClick={() => setMenuActive()}>
           <p className={`${MenuScreenStyles.menuItem} ${menuActive ? MenuScreenStyles.itemActive : null}`}>About</p>
         </Link>
         <Link to="/projects" onClick={() => setMenuActive()}>
           <p className={`${MenuScreenStyles.menuItem} ${menuActive ? MenuScreenStyles.itemActive : null}`}>Work</p>
         </Link>
-        <Link to="/#contact">
+        <Link to="/#contact" onClick={() => setMenuActive()}>
           <p className={`${MenuScreenStyles.menuItem} ${menuActive ? MenuScreenStyles.itemActive : null}`}>Contact</p>
         </Link>
       </div>
       <div className={MenuScreenStyles.social}>
-        <FontAwesomeIcon icon={faTwitter} />
-        <FontAwesomeIcon icon={faGithub} />
-        <FontAwesomeIcon icon={faMediumM} />
+        <a href="https://twitter.com/_JamesCoates" target="blank"><FontAwesomeIcon icon={faTwitter} /></a>
+        <a href="https://github.com/James-Coates" target="blank"><FontAwesomeIcon icon={faGithub} /></a>
+        <a href="https://medium.com/@jamescoates192" target="blank"><FontAwesomeIcon icon={faMediumM} /></a>
       </div>
     </div>
     <div className={MenuScreenStyles.col2}>
