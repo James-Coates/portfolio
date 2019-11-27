a6ab11775dbf36923844632d8b5afb05bfe88172
james <james.coates@me.com>
2019-11-27 21:22:32 +0000

    Updates

--- component---src-pages-contact-js-3cc0e2dcfe088286a804.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{192:function(n,t,e){"use strict";e.r(t);var c=e(0),a=e.n(c);t.default=function(n){return a.a.createElement("div",null,"contact")}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-3cc0e2dcfe088286a804.js.map
---
+++ component---src-pages-contact-js-a1b9ed13ca13e585e1e8.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{233:function(n,t,e){"use strict";e.r(t);var c=e(0),a=e.n(c);t.default=function(n){return a.a.createElement("div",null,"contact")}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-a1b9ed13ca13e585e1e8.js.map
